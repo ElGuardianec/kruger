c5dc4f606abe762764be8acf1d22b01a9d715a58
ElGuardianec <52641606+ElGuardianec@users.noreply.github.com>
2021-09-15 14:20:56 -0500

    first commit

---
+++ src/test/java/com/vacunakruger/kruger/KrugerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacunakruger.kruger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrugerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
